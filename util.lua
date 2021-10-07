b840cea30cfeb718b3fefee7abf63fe55ce520de
Nuno Das Neves <ndasneves@gmail.com>
2021-10-06 21:33:27 -0700

    move aabb to util

--- util.lua
+++ util.lua
@@ -11,3 +11,17 @@ end
 function rounddown(val, multiple)
     return val - val % multiple
 end
+
+function aabb(x0,y0,w0,h0,x1,y1,w1,h1)
+    local r0 = x0 + w0
+    local r1 = x1 + w1
+    local b0 = y0 + h0
+    local b1 = y1 + h1
+    if (r0 < x1 or r1 < x0) then
+        return false
+    end
+    if (b0 < y1 or b1 < y0) then
+        return false
+    end
+    return true
+end
